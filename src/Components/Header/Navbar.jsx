cfd7130cf7ea5f8133b1570f5dfc2a868f91c970
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-11 05:25:14 +0600

    Added font family and create custom font for tailwind class

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar=()=> {
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
             <div className='flex items-center'>
-                <p className='text-4xl'><GrWorkshop></GrWorkshop></p>
-                <h4 className='text-4xl'>Jobs<span>Placer</span></h4>
+                <p className='text-4xl text-[#11B719]'><GrWorkshop></GrWorkshop></p>
+                <h4 className='text-4xl font-lora'>Jobs<span className='text-[#11B719]'>Placer</span></h4>
             </div>
 
             {/* Mobile menu button */}
@@ -40,12 +40,7 @@ const Navbar=()=> {
             </div>
 
             <div className="flex items-center mt-4 lg:mt-0">
-              <button className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none" aria-label="show notifications">
-                <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-              </button>
-
+              
               <button type="button" className="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
                 <div className="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
                   <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" className="object-cover w-full h-full" alt="avatar" />
