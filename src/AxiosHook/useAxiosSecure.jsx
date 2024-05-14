7afd1b12d46b1d55cfcdd94b808a152a6e071ac0
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-14 22:57:56 +0600

    added interceptor for handling error for secure data fetch

--- src/AxiosHook/useAxiosSecure.jsx
+++ src/AxiosHook/useAxiosSecure.jsx
@@ -1,10 +1,30 @@
 import axios from "axios";
+import useAuth from "../Auth/AuthHook/useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure=axios.create({
     baseURL:import.meta.env.VITE_LOCAL_URL,
     withCredentials:true
 })
 const useAxiosSecure = () => {
+    const {logOutUser} =useAuth();
+    const navigate=useNavigate();
+    useEffect(()=>{
+        axiosSecure.interceptors.response.use(res=>{
+            return res;
+        },
+    error=>{
+        // console.log(error.response);
+        // console.log("Error track by interceptor",error.response);
+        if(error.response.status ===401 || error.response.status ===403){
+            logOutUser()
+            .then(()=>{
+                navigate("/loginUser")
+            })
+        }
+    })
+    },[navigate,logOutUser])
     return axiosSecure;
 };
 
